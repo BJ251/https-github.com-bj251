371ec756387101ec6aabfcaae132dde4b08d4963
BJ251 <cesar.barrios.502@gmail.com>
2019-12-30 21:18:50 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+      Scanner keyboard = new Scanner(System.in);
+    
+    System.out.println("Hello world!");
+    System.out.print("Enter number 1: ");
+    int one = keyboard.nextInt();
+    System.out.print("Enter number 2: ");
+    int two = keyboard.nextInt();
+    System.out.print("Enter number 3: ");
+    int three = keyboard.nextInt();
+
+    System.out.println("Chose an Option : ");
+    
+
+
+  }
+}
